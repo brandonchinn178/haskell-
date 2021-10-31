d88ca89c04e5fac5e561b9bc9fa2b246a045fcbd
Brandon Chinn <brandon@leapyear.io>
2021-10-31 15:25:26 -0700

    Refactor with one graph-traversing loop

--- generate.py
+++ generate.py
@@ -12,8 +12,6 @@ https://forum.sublimetext.com/t/syntax-definition-explicitly-specify-backref-con
 """
 
 import yaml
-from dataclasses import dataclass
-from functools import cached_property
 from pathlib import Path
 
 HERE = Path(__file__).parent
@@ -28,124 +26,82 @@ INDENTATION_MARKER = "{{INDENTATION}}"
 def main():
     # read in old data
     data = decode_yaml(TEMPLATE.read_text())
-    contexts = ContextRegistry.parse(data)
+    old_contexts = data["contexts"]
 
-    new_contexts = {}
-    copied_contexts = set()
-    def _add_new_context(name, patterns, indent=None):
-        new_name = name_with_indent(name, indent) if indent is not None else name
-        new_contexts[new_name] = patterns
-        copied_contexts.add(name)
-
-    # duplicate all indentation-introducing match patterns
-    indent_introducing_patterns = []
-    for context in contexts.all_contexts:
-        if not context.starts_indent:
-            continue
-
-        new_patterns = []
-        for pattern in context.patterns:
-            pattern_match = pattern.get("match", "")
-
-            if INDENTATION_MARKER not in pattern_match:
-                new_patterns.append(pattern)
-                continue
-
-            new_patterns.extend(
-                {
-                    **pattern_with_indent(pattern, i),
-                    "match": pattern_match.replace(
-                        INDENTATION_MARKER,
-                        # special case (i=0); '\s{0}' doesn't seem to work here
-                        r"(?!\s)" if i == 0 else indent_regex(i),
-                    ),
-                }
-                for i in INDENTATIONS
-            )
-            indent_introducing_patterns.append(pattern)
-
-        _add_new_context(context.name, new_patterns)
-
-    # find all contexts to duplicate
-    contexts_to_duplicate = set()
-    seen = set()
-    queue = [
-        (context, [])
-        for pattern in indent_introducing_patterns
-        for context in contexts_in_pattern(pattern)
-    ]
-    while len(queue) > 0:
-        context_name, path = queue.pop(0)
-        if context_name in seen:
-            continue
-
-        seen.add(context_name)
-        path.append(context_name)
-
-        context = contexts.get_context(context_name)
-        if context.ends_indent:
-            contexts_to_duplicate.update(path)
-
-        queue.extend(
-            (c, path)
-            for c in context.children
-        )
+    # find all indented contexts
+    indented_contexts = get_indented_contexts(old_contexts)
 
     # find all branch points within duplicated context
     branch_points_to_duplicate = set()
-    for context_name in contexts_to_duplicate:
-        context = contexts.get_context(context_name)
-        for pattern in context.patterns:
+    for context in indented_contexts:
+        for pattern in old_contexts[context]:
             branch_point = pattern.get("branch_point")
             if branch_point:
                 branch_points_to_duplicate.add(branch_point)
 
-    # duplicate "pop_when_deindent" manually
-    for i in INDENTATIONS:
-        _add_new_context(
-            name="pop_when_deindent",
-            patterns=[
-                {
-                    "match": r"^(?!%s\s)" % indent_regex(i),
-                    "pop": True,
-                },
-            ],
-            indent=i,
-        )
+    new_contexts = {}
+    def _add_new_context(context_name, indent, patterns):
+        if indent is not None:
+            context_name = name_with_indent(context_name, indent)
+        new_contexts[context_name] = patterns
 
-    # duplicate all contexts between a context introducing indentation and a
-    # context that pops indentation
-    for context_name in contexts_to_duplicate:
-        context = contexts.get_context(context_name)
-        for i in INDENTATIONS:
+    for context, patterns in old_contexts.items():
+        # duplicate "pop_when_deindent" manually
+        if context == "pop_when_deindent":
+            for i in INDENTATIONS:
+                _add_new_context(context, i, [
+                    {
+                        "match": r"^(?!%s\s)" % indent_regex(i),
+                        "pop": True,
+                    },
+                ])
+            continue
+
+        indentations = INDENTATIONS if context in indented_contexts else [None]
+        for indent in indentations:
             new_patterns = []
-            for pattern in context.patterns:
-                new_pattern = pattern_with_indent(pattern, i)
 
+            def _pattern_with_indent(pattern, indent):
+                if indent is None:
+                    return pattern
+
+                new_pattern = pattern_with_indent(pattern, indent)
+
+                # TODO(nested-indent): generalize this to set indentation of branch_point
+                # to the appropriate indentation, instead of the current indentation
                 branch_point = new_pattern.get("branch_point")
                 if branch_point and branch_point in branch_points_to_duplicate:
-                    new_pattern["branch_point"] = name_with_indent(branch_point, i)
+                    new_pattern["branch_point"] = name_with_indent(branch_point, indent)
 
                 branch_fail = new_pattern.get("fail")
                 if branch_fail and branch_fail in branch_points_to_duplicate:
-                    new_pattern["fail"] = name_with_indent(branch_fail, i)
+                    new_pattern["fail"] = name_with_indent(branch_fail, indent)
 
-                new_patterns.append(new_pattern)
+                return new_pattern
 
-            _add_new_context(
-                name=context_name,
-                patterns=new_patterns,
-                indent=i,
-            )
+            for pattern in patterns:
+                pattern_match = pattern.get("match", "")
+                if INDENTATION_MARKER in pattern_match:
+                    # TODO(nested-indent): handle when `indent is not None`
+                    for indent_inner in INDENTATIONS:
+                        new_pattern = _pattern_with_indent(pattern, indent_inner)
+                        new_pattern["match"] = pattern_match.replace(
+                            INDENTATION_MARKER,
+                            # special case; '\s{0}' doesn't seem to work here
+                            r"(?!\s)" if indent_inner == 0 else indent_regex(indent_inner),
+                        )
+                        new_patterns.append(new_pattern)
+                else:
+                    new_pattern = _pattern_with_indent(pattern, indent)
+                    new_patterns.append(new_pattern)
 
-    # copy everything else over
-    for context in contexts.all_contexts:
-        if context.name not in copied_contexts:
-            _add_new_context(context.name, data["contexts"][context.name])
+            _add_new_context(context, indent, new_patterns)
 
     # write out new data
     data["contexts"] = new_contexts
     OUTPUT.write_text(encode_yaml(data))
+
+### Indentation strings ###
 
 def name_with_indent(name: str, indent: int) -> str:
     # TODO(extraneous): remove this manual workaround
@@ -166,42 +122,83 @@ def name_with_indent(name: str, indent: int) -> str:
 def indent_regex(indent: int) -> str:
     return r"\s{%d}" % indent
 
-### Pattern ###
+### Finding indented contexts ###
 
-# TODO: visitor pattern?
-def contexts_in_pattern(pattern: dict) -> set[str]:
-    contexts = set()
+Pattern = dict
+Patterns = list[Pattern]
+ContextName = str
+
+def get_indented_contexts(contexts: dict[ContextName, Patterns]) -> set[ContextName]:
+    indented_contexts = set()
+
+    context_queue = [("main", None), ("prototype", None)]
+    seen = set()
+    while len(context_queue) > 0:
+        context, path = context_queue.pop(0)
+
+        if context in indented_contexts:
+            indented_contexts.update(path or [])
+            continue
+
+        # check cycles
+        node = (context, None if path is None else tuple(path))
+        if (node in seen) or (path and context in path):
+            continue
+        seen.add(node)
+
+        path = None if path is None else path + [context]
+        for pattern in contexts[context]:
+            if pattern.get("include") == "pop_when_deindent":
+                indented_contexts.update(path or [])
+
+            # TODO(nested-indent): when matching indentation within indented context,
+            # nest indentation indicators; e.g. `function_signature_start__2__4`, so
+            # that we can use 'function__2' as a branch point
+            if INDENTATION_MARKER in pattern.get("match", "") and path is None:
+                next_path = []
+            else:
+                next_path = path
+
+            context_queue.extend(
+                (next_context, next_path)
+                for next_context in get_contexts_in_pattern(pattern)
+            )
+
+    return indented_contexts
+
+def get_contexts_in_pattern(pattern: Pattern) -> list[ContextName]:
+    contexts = []
 
     pattern_include = pattern.get("include")
     if pattern_include:
-        contexts.add(pattern_include)
+        contexts.append(pattern_include)
 
     pattern_match = pattern.get("match")
     if pattern_match:
         pattern_embed = pattern.get("embed")
         if pattern_embed:
-            contexts.add(pattern_embed)
+            contexts.append(pattern_embed)
 
-        pattern_branches = pattern.get("branch", [])
-        contexts.update(pattern_branches)
+        contexts.extend(pattern.get("branch", []))
 
         for key in ["push", "set"]:
             pattern_next = pattern.get(key)
             if pattern_next is None:
                 continue
             if isinstance(pattern_next, str):
-                contexts.add(pattern_next)
+                contexts.append(pattern_next)
             elif all(isinstance(p, str) for p in pattern_next):
-                contexts.update(pattern_next)
+                contexts.extend(pattern_next)
             else:
-                contexts.update(
-                    context
-                    for p in pattern_next
-                    for context in contexts_in_pattern(p)
+                contexts.extend(
+                    p
+                    for pattern in pattern_next
+                    for p in get_contexts_in_pattern(pattern)
                 )
 
     return contexts
 
+### Pattern ###
 
 # TODO(extraneous): fix when this context is duplicated but pushed context isn't
 def pattern_with_indent(pattern: dict, indent: int) -> dict:
@@ -251,93 +248,6 @@ def pattern_with_indent(pattern: dict, indent: int) -> dict:
         return new_pattern
 
     return pattern
-
-### Contexts ###
-
-@dataclass
-class ContextInfo:
-    name: str
-    starts_indent: bool  # has a pattern that matches on indentation level
-    ends_indent: bool  # includes pop_when_deindent
-    children: list[str]
-    patterns: list
-
-    @classmethod
-    def parse(cls, name: str, patterns: list[dict]) -> "ContextInfo":
-        context = cls(
-            name=name,
-            starts_indent=False,
-            ends_indent=False,
-            children=list({
-                context
-                for pattern in patterns
-                for context in contexts_in_pattern(pattern)
-            }),
-            patterns=patterns,
-        )
-
-        patterns_queue = context.patterns[:]
-        while len(patterns_queue) > 0:
-            pattern = patterns_queue.pop(0)
-
-            pattern_include = pattern.get("include")
-            if pattern_include:
-                if pattern_include == "pop_when_deindent":
-                    context.ends_indent = True
-
-            pattern_match = pattern.get("match")
-            if pattern_match:
-                if INDENTATION_MARKER in pattern_match:
-                    context.starts_indent = True
-
-                pattern_push = pattern.get("push")
-                pattern_set = pattern.get("set")
-                for pattern_next in [pattern_push, pattern_set]:
-                    if pattern_next is None:
-                        continue
-                    if isinstance(pattern_next, str):
-                        pass
-                    elif all(isinstance(p, str) for p in pattern_next):
-                        pass
-                    else:
-                        patterns_queue = pattern_next + patterns_queue
-
-        return context
-
-class ContextRegistry:
-    def __init__(self):
-        self._contexts = {}  # type: dict[str, ContextInfo]
-
-    def __repr__(self):
-        return repr(self.all_contexts)
-
-    @cached_property
-    def all_contexts(self) -> list[ContextInfo]:
-        return list(self._contexts.values())
-
-    def get_context(self, name: str) -> ContextInfo:
-        return self._contexts[name]
-
-    @classmethod
-    def parse(cls, data: dict) -> "ContextRegistry":
-        contexts = cls()
-
-        seen = set()
-        queue = ["main", "prototype"]
-        while len(queue) > 0:
-            context_name = queue.pop(0)
-            if context_name in seen:
-                continue
-
-            seen.add(context_name)
-            context = ContextInfo.parse(
-                name=context_name,
-                patterns=data["contexts"][context_name],
-            )
-            contexts._contexts[context_name] = context
-            queue.extend(context.children)
-
-        return contexts
 
 ### YAML ###
 
